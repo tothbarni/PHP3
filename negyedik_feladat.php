bc651d4b0b54e21184f1be134e67793666fa9bb4
Barni <tothbarni.06@gmail.com>
2025-03-14 07:52:25 +0100

    phporai

--- negyedik_feladat.php
+++ negyedik_feladat.php
@@ -1,3 +1,7 @@
+<?php
+    include "backend.php"
+?>
+
 <!DOCTYPE html>
 <html lang="hu">
 <head>
@@ -13,17 +17,27 @@
         <h1>Hogyan takarékoskodhatunk a vízzel?</h1>
         <?php
             include 'fuggvenyek.php';
-            bekezedes("A Föld édesvízkészlete véges, hogyan takarékoskodhatunk a vízzel?");
-            echo "Az ivóvíz születéséért rengeteget dolgoznak a vízcseppek és az emberek is. Ráadásul a felhasználható víz mennyisége sem korlátlan. Ezért mondják a víztörténetét ismerő felnőttek, hogy óvd a természetet és takarékoskodj a vízzel. Fürdés helyett inkább zuhanyozz! Egészségesebb és takarékosabb is, mint a kádban fürdés. Ne folyasd feleslegesen a vizet, a fogmosáshoz használj poharat! Ha csöpögő csapot látsz, szólj a felnőtteknek! A mosógépet rakjuk tele ruhával, ha beindítjuk, egy-két pólóért pazarlás beindítani. De azzal is vizet spórolunk meg, ha vigyázunk a ruháink tisztáságára. Ha kevesebb ruhát piszkolunk össze, kevesebb lesz a mosnivalónk is. Ügyeljünk arra, hogy sáros lábbal ne menjünk be a lakásba, mert úgy is megspórolhatunk vizet, ha nem kell naponta többször felmosnunk. A kertet reggel és este locsold, ilyenkor kevésbé párolog a víz. De locsolásra használhatjuk az összegyűjtött esővizet is. A vizet a növények gyökeréhez juttasd!
+            $bekezedes("A Föld édesvízkészlete véges, hogyan takarékoskodhatunk a vízzel?");
+        ?>  
+            Az ivóvíz születéséért rengeteget dolgoznak a vízcseppek és az emberek is. Ráadásul a felhasználható víz mennyisége sem korlátlan. Ezért mondják a víztörténetét ismerő felnőttek, hogy óvd a természetet és takarékoskodj a vízzel. Fürdés helyett inkább zuhanyozz! Egészségesebb és takarékosabb is, mint a kádban fürdés. Ne folyasd feleslegesen a vizet, a fogmosáshoz használj poharat! Ha csöpögő csapot látsz, szólj a felnőtteknek! A mosógépet rakjuk tele ruhával, ha beindítjuk, egy-két pólóért pazarlás beindítani. De azzal is vizet spórolunk meg, ha vigyázunk a ruháink tisztáságára. Ha kevesebb ruhát piszkolunk össze, kevesebb lesz a mosnivalónk is. Ügyeljünk arra, hogy sáros lábbal ne menjünk be a lakásba, mert úgy is megspórolhatunk vizet, ha nem kell naponta többször felmosnunk. A kertet reggel és este locsold, ilyenkor kevésbé párolog a víz. De locsolásra használhatjuk az összegyűjtött esővizet is. A vizet a növények gyökeréhez juttasd!
             <p>A folyadékáram jellemző adata a folyadék áramlási sebessége (v). Az áramló folyadék mennyiségét a térfogatárammal fejezzük ki. A térfogatáram egy adott keresztmetszeten időegység alatt átáramló folyadék térfogata. A térfogatáram számítása: V = v × A ahol: </p>
             <p>V az adott keresztmetszeten időegység alatt átáramló folyadék térfogata, mértékegysége: m3/s  ,</p>
             <p>v a folyadék áramlási sebessége, mértékegysége: m/s ,</p>
             <p>A a folyadékáram keresztmetszete, mértékegysége: m2.</p>
-            <p>V folyadékáram :  <input type='number'></p>
-            <p>A folyadékáram áramlási sebessége :  <input type='number'></p>
-            ";
             
-        ?>  
+            <form action="negyedik_feladat.php" method="post">
+                <p>V folyadékáram : <input type="number" name="A" value="1"></p>
+                <p>A folyadékáram áramlási sebessége : <input type="number" name="v" value="1"></p>
+                <input type="submit" name="submit" class="submit" value="szamol">
+            </form>
+            <?php
+                if (isset($_POST['submit'])) {
+                    $v = $_POST['v'];
+                    $A = $_POST['A'];
+                    $terfogat = $v * $A;
+                    echo "<p>Az adott keresztmetszeten időegység alatt átáramló folyadék térfogata: $terfogat köbméter/másodperc.</p>";
+                }
+            ?>
     </main>
 </body>
 </html>
